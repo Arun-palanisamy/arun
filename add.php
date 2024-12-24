611af8e2d4a69c79f2ec520928cf2bcb32f2eae3
Arun-palanisamy <arunp.24cse@kongu.edu>
2024-12-24 11:41:00 +0530

    second project

---
+++ add.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Addition in PHP</title>
+</head>
+<body>
+    <h2>Addition Form</h2>
+    <form method="post" action="">
+        <label for="num1">Number 1:</label><br>
+        <input type="number" id="num1" name="num1" required><br><br>
+        <label for="num2">Number 2:</label><br>
+        <input type="number" id="num2" name="num2" required><br><br>
+        <input type="submit" value="Add">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+        $sum = $num1 + $num2;
+
+        echo "<h2>Result</h2>";
+        echo "The sum of " . htmlspecialchars($num1) . " and " . htmlspecialchars($num2) . " is " . htmlspecialchars($sum) . ".";
+    }
+    ?>
+</body
